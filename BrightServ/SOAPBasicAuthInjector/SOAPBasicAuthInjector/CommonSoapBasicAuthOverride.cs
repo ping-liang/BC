615bc2b9a9bef95e69d13984ae0b35b0f1bea5bd
Ping Liang <ping.liang08@gmail.com>
2018-02-22 17:40:11 -0500

    Added SOABasicAuthInjector

---
+++ BrightServ/SOAPBasicAuthInjector/SOAPBasicAuthInjector/CommonSoapBasicAuthOverride.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace SOAPBasicAuthInjector.CommonOverride
+{
+	internal static class WebRequestCustom
+	{
+		internal static WebRequest GetWebRequestCustom(HttpWebRequest request, NetworkCredential networkCredentials )
+		{
+			if (networkCredentials != null)
+			{
+				byte[] credentialBuffer = new UTF8Encoding().GetBytes(
+					networkCredentials.UserName + ":" +
+					networkCredentials.Password);
+				request.Headers["Authorization"] =
+					"Basic " + Convert.ToBase64String(credentialBuffer);
+			}
+			else
+			{
+				throw new ApplicationException("No network credentials");
+			}
+			return request;
+		}
+	}
+}
+
+namespace SOAPBasicAuthInjector.GenPactUserInfo
+{
+	public partial class EmpServicePort_GetUserName_REQUEST
+	{
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}         
+    }
+}
+
+namespace SOAPBasicAuthInjector.AR_CreateInvoice
+{
+	public partial class InoviceData_createInvoice_REQUEST
+	{
+		protected override WebRequest GetWebRequest(Uri uri)
+		{
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}
+	}
+}
+
+namespace SOAPBasicAuthInjector.AR_CreditNote
+{
+	public partial class CreditNote_CreateCredit_REQUEST
+	{
+		protected override WebRequest GetWebRequest(Uri uri)
+		{
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}
+	}
+}
+
+namespace SOAPBasicAuthInjector.AR_GetOracleInvoiceNumber
+{
+	public partial class OracleInvoiceNumber_Oracleinovice_REQUEST
+	{
+		protected override WebRequest GetWebRequest(Uri uri)
+		{
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}
+	}
+}
+
+namespace SOAPBasicAuthInjector.AR_GetOutstandingAmount
+{
+	public partial class GetInvoiceStatus_Invoice_Status_REQUEST
+	{
+		protected override WebRequest GetWebRequest(Uri uri)
+		{
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}
+	}
+}
+
+namespace SOAPBasicAuthInjector.AP_CreateCase
+{
+	public partial class Create_Case_From_PNMSoft_Akritiv_REQUESTPortType_CreateCase_REQUEST
+	{
+		protected override WebRequest GetWebRequest(Uri uri)
+		{
+			return CommonOverride.WebRequestCustom.GetWebRequestCustom((HttpWebRequest)base.GetWebRequest(uri), Credentials.GetCredential(uri, "Basic"));
+		}
+	}
+}
+
