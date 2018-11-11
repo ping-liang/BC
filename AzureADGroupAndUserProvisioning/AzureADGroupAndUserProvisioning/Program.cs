302627116d3301a1269fb42b5291c040bb49c016
Ping Liang <ping.liang08@gmail.com>
2018-11-11 04:14:16 -0500

    te

---
+++ AzureADGroupAndUserProvisioning/AzureADGroupAndUserProvisioning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureADGroupAndUserProvisioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
